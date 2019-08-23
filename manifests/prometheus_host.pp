93dec5474f10f250be2373e9b2cff1cc51e8f6f4
Zype76 <zype76@gmail.com>
2019-08-22 23:20:37 -0400

    updates

--- manifests/prometheus_host.pp
+++ manifests/prometheus_host.pp
@@ -1,8 +1,24 @@
 class prometheus_host () {
+  #Create a directory and user for it
+  group { 'prometheus':
+    ensure => 'present',
+  }
+  user { 'prometheus':
+    ensure           => 'present',
+    group            => 'prometheus',
+    password_max_age => '99999',
+    password_min_age => '0',
+    shell            => '/bin/bash',
+    require => Group['prometheus'],
+  }
+
   file { '/opt/prometheus' :
     ensure => directory,
+    owner  => 'prometheus',
+    require => User['prometheus'],
   }
-  
+
+  #Install prometheus and configure service
   exec { 'wget -P /tmp/ https://github.com/prometheus/prometheus/releases/download/v2.12.0/prometheus-2.12.0.linux-amd64.tar.gz':
     creates => '/root/alreadydonelel',
     path    => ['/usr/bin', '/usr/sbin',],
